538e31bc13b1451f7e6d3975a5d1dafff0604a55
Zach Ehmann <zach.ehmann@icalliances.org>
2023-07-21 14:24:56 -0400

    M4 initial universe definition: stayer/leaver + partial income clean
    Set up initial transformations for defining M4 universe. Split into stayer + leaver groups based on whether active on last report date.
    
    Began filtering the income data for stayers based on annual assessment records in range of the relevant Entry anniversary date. Then deduplicated AA's by Enrollment-Anniversary Year pairs to avoid using AAs from the same year for both start and end measurement points.
    
    Next step is to establish start/end measurement points based on most recent qualifying AA and the previous qualifying AA (or entry, if none). If the entry is is missing income data, case must be excluded from measure.
    
    Need to establish what constitutes "missing" income value -- there are cases where Enrollment is represented on IncomeBenefits table but record is otherwise NULL. These either need to be filtered earlier or be triggers for categorizing cases as "missing information." Seek ICF TA clarification.

--- System Performance Measures/system_performance_measures_4.R
+++ System Performance Measures/system_performance_measures_4.R
@@ -9,7 +9,7 @@
 # GNU Affero General Public License for more details at
 # <https://www.gnu.org/licenses/>. 
 
-### IF RUNNING STANDALONE: run system_performance_measures_0.R through line 118 first ###
+### IF RUNNING STANDALONE: run system_performance_measures_0.R through line 118 FIRST ###
 
 # Measure 4: Employment and Income Growth for Homeless Persons in CoC Program-funded Projects
 
@@ -46,37 +46,119 @@ M4_stayer_clients <- M4_enrollment_u %>%
 
 M4_stayers <- M4_enrollment_u %>%
   filter(
-    .$PersonalID %in% M4_stayer_clients, 
+    PersonalID %in% M4_stayer_clients, 
     difftime( report_end_date , EntryDate, units = "days") >= 365
     ) %>%
   arrange(PersonalID, EntryDate, EnrollmentID) %>%
   group_by(PersonalID) %>%
   filter(row_number()==1) %>%
   mutate(
-    Age = floor(age_calc( DOB, max( report_start_date, EntryDate), units = "years"))
+    age = floor(age_calc( DOB, max( report_start_date, EntryDate), units = "years"))
   ) %>%
   ungroup() %>% 
-  filter(Age >= 18)
+  filter(age >= 18) %>% #end universe filtering
+  # start prep calculations for income lookup
+  mutate(
+    years_enrolled = floor(age_calc( EntryDate, report_end_date, units = "years")),
+    report_anniversary = EntryDate %m+% years(years_enrolled),
+    report_anniversary_start = report_anniversary %m-% days(30),
+    report_anniversary_end = report_anniversary %m+% days(30),
+    annual_deadline_in_report = (report_anniversary_end %m+% days(1)) <= report_end_date 
+  )
   
 # leavers
 
 M4_leavers <- M4_enrollment_u %>%
   filter(
-    .$PersonalID %nin% M4_stayer_clients,
+    PersonalID %nin% M4_stayer_clients,
     ExitDate >= report_start_date & ExitDate <= report_end_date
   ) %>%
   arrange(PersonalID, EntryDate, EnrollmentID) %>%
   group_by(PersonalID) %>%
   filter(row_number()==1) %>%
   mutate(
-    Age = floor(age_calc( DOB, max( report_start_date, EntryDate), units = "years"))
+    age = floor(age_calc( DOB, max( report_start_date, EntryDate), units = "years"))
   ) %>%
   ungroup() %>% 
-  filter(Age >= 18)
+  filter(age >= 18)
 
 ## Lookup income start + end records ----
 
-# 
+#### QUESTIONS
+  ## How to pull in info before lookback_stop_date for old records?
+  ## Specs are garbled for step "g" under stayers. But I'm assuming intention
+    ## is to identify the AA *previous* to the most recent AA. *LEFT COMMENT*
+  ## How to handle >0 amount but "No" for income source?
 
+#### datalab functions referenced
+  ## APR/CAPER
+    ##get_annual_id
+
+# global income prep
+### ADD THIS BACK AFTER reduce the size of the table
+
+# base_income <- IncomeBenefits %>%
+#   # mutate(across(c(Earned, Unemployment, SSI, SSDI,
+#   VADisabilityService, VADisabilityNonService,
+# PrivateDisability, WorkersComp, TANF, GA,
+# SocSecRetirement, Pension, ChildSupport,
+# Alimony, OtherIncomeSource), ~ ifnull(., 0))
+
+# stayer income
+
+stayer_income <- IncomeBenefits %>%
+  filter(
+    EnrollmentID %in% M4_stayers$EnrollmentID
+  )
+
+stayer_income_entry <- stayer_income %>% filter(DataCollectionStage == 1)
+
+stayer_income_annuals <- stayer_income %>% 
+  filter(DataCollectionStage == 5) %>%
+  left_join(
+    M4_stayers %>% select(EnrollmentID, EntryDate),
+    by = "EnrollmentID"
+  ) %>% 
+  # these identify the relevant anniversary date and associated range 
+    # for each existing Annual Assessment and checks to see if within
+    # the +-30 day range.
+  mutate(
+    years_entry_to_aa = floor(age_calc( EntryDate %m-% days(30), InformationDate, units = "years")),
+    related_anniversary_date = EntryDate %m+% years(years_entry_to_aa),
+    in_range = 
+      trunc((related_anniversary_date %--% InformationDate) / days(1)) >= -30 &
+      trunc((related_anniversary_date %--% InformationDate) / days(1)) <= 30
+    ) %>%
+  # used following select step for QA
+  #select( EnrollmentID, IncomeBenefitsID, EntryDate, InformationDate, years_entry_to_aa, related_anniversary_date, in_range) %>%
+  
+  filter(in_range == TRUE) %>%
+  
+  # remove duplicate in-range AAs for the same anniversary date
+    ## there weren't any cases in Iowa BoS test data, but still think should account
+      ## for this possibility
+  arrange(EnrollmentID, desc(related_anniversary_date), desc(InformationDate), desc(IncomeBenefitsID)) %>%
+  select( EnrollmentID,related_anniversary_date, InformationDate, IncomeBenefitsID) %>%
+  group_by(EnrollmentID, related_anniversary_date) %>% 
+  filter(row_number() == 1) %>% 
+  ungroup() %>%
+  group_by(EnrollmentID) %>%
+  mutate(
+    count_annuals_by_EE = n()
+  ) %>%
+  ungroup()
+
+
+### ISSUE -- Need to determine if NULL Income/Benefit records are to be removed 
+  ### and if this should be done prior to other filter steps.
+
+    ## OLD VERSION -- going back 30 days in all cases should be OK
+    # years_entry_to_aa = function(x){
+    #   x = floor(age_calc( EntryDate, InformationDate, units = "years"))
+    #   if(x < 1) {
+    #     floor(age_calc( EntryDate %m-% days(30), InformationDate, units = "years"))
+    #   } else {x}
+    # }
+  
 
 
